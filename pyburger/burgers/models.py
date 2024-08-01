15c826431c2d676a5dc534e7cc3d808c10452d8f
이상용 <lsy3709@naver.com>
2024-08-01 16:05:56 +0900

    6교시, urls 버거리스트 호출해서 화면에서 확인해보기3

--- pyburger/burgers/models.py
+++ pyburger/burgers/models.py
@@ -1,6 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+# 앱만들기 명령어
+# python manage.py startapp burgers
+
 #실제로 데이터베이스 반영이되는 클래스,
 # 스프링, 엔티티 클래스와 동일함.
 # 클래스를 정의를 하면, 자동으로 테이블을 생성해줌. ORM
